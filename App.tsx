1ab7fde011ab4646dfe83c828f1afdd1465a84b4
Nicolas Rausch <122921640+nioark@users.noreply.github.com>
2025-05-11 18:00:59 -0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,18 +1,25 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 
 const App = () => {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: 'purple',
-      }}>
-      <Text>Hello, world!</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello, world!</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'purple',
+  },
+  text: {
+    fontSize: 20,
+    color: 'white',
+  },
+});
 
 export default App;
